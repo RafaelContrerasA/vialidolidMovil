cb74d7522c3e5ae83be5485992c16109852abf14
Franco <leonardofranco3100@gmail.com>
2023-05-11 10:12:39 -0600

    Interfaces reporte alumbrado, bache, completo, maltrato, suministro, vial

---
+++ app/src/main/java/com/example/vialidolid/reporte_alumbrado_publico.kt
@@ -0,0 +1,11 @@
+package com.example.vialidolid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reporte_alumbrado_publico : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reporte_alumbrado_publico)
+    }
+}
---
+++ app/src/main/java/com/example/vialidolid/reporte_vial.kt
@@ -0,0 +1,11 @@
+package com.example.vialidolid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reporte_vial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reporte_vial)
+    }
+}
