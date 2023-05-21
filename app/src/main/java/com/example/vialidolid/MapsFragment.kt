9ef0c1f63e551d8cd4ab379dd6bb6aaf48f0e835
Rafael Alejandro Contreras Avalos <l20120970@morelia.tecnm.mx>
2023-05-20 19:54:38 -0600

    Agregar ubicacion actual al mapa, solo de forma estatica al presionar un boton

--- app/src/main/java/com/example/vialidolid/MapsFragment.kt
+++ app/src/main/java/com/example/vialidolid/MapsFragment.kt
@@ -1,5 +1,9 @@
 package com.example.vialidolid
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -7,6 +11,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,8 +21,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+import androidx.appcompat.app.AlertDialog
+import android.provider.Settings
+
 class MapsFragment : Fragment() {
     val coordenadas = mutableMapOf<String, LatLng>()
+
+    private lateinit var map : GoogleMap
+    private var LOCATION_PERMISSION_REQUEST = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val coordenada1 = LatLng(-33.9, 151.0)
@@ -38,6 +50,7 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+        map = googleMap
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
@@ -50,6 +63,7 @@ class MapsFragment : Fragment() {
         googleMap.addMarker(MarkerOptions().position(coordenadas["Coordenada 1"] as LatLng).title("Marker in Sydney"))
         googleMap.addMarker(MarkerOptions().position(coordenadas["Coordenada 2"] as LatLng).title("Marker in Sydney"))
 
+        getLocationAccess()
     }
 
     override fun onCreateView(
@@ -64,6 +78,59 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    private fun getLocationAccess() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            map.isMyLocationEnabled = true
+            checkLocationEnabled()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST
+            )
+        }
+    }
+
+    private fun checkLocationEnabled() {
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val isLocationEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (!isLocationEnabled) {
+            showEnableLocationDialog()
+        }
+    }
+
+    private fun showEnableLocationDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("GPS Desactivado")
+            .setMessage("Para utilizar la función de ubicación, necesitas activar el GPS.")
+            .setPositiveButton("Ir a Configuración") { _, _ ->
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancelar") { dialog, _ ->
+                dialog.dismiss()
+            }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,  grantResults:
+    IntArray) {
+        if(requestCode == LOCATION_PERMISSION_REQUEST){
+            if(grantResults.contains(PackageManager.PERMISSION_GRANTED)){
+                map.isMyLocationEnabled = true
+            }
+            else{
+                Toast.makeText(requireContext(),"No se proporcionó permisos", Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 
 
