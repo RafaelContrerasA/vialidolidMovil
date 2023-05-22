031f78e53bdb9f5142bd6d070295ffc5f8f902b8
Rafael Alejandro Contreras Avalos <l20120970@morelia.tecnm.mx>
2023-05-22 17:08:36 -0600

    Implementacion primitiva de obtener ubicacion para el reporte

--- app/src/main/java/com/example/vialidolid/reporte_bache.kt
+++ app/src/main/java/com/example/vialidolid/reporte_bache.kt
@@ -3,23 +3,45 @@ package com.example.vialidolid
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.*
+import kotlin.collections.HashMap
 
 class reporte_bache : AppCompatActivity() {
-    var etUbicacion: EditText? = null
     var etDescripcion: EditText? = null
     var etReferencias: EditText? = null
     var gpsActivado = true
+
+    //Obtener las coordenadas actuales y colocarlas dentro del editText
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    private var etUbicacion: EditText? = null
+    private lateinit var geocoder: Geocoder
+
+    //Ubicacion por defecto en caso de no poder obtener la ubicacion del usuario.
+    private var lat: Double = 19.7040
+    private var lng: Double = -101.1908
+    private var calle : String? = "Guillermo Prieto 314"
+    private var colonia : String? = "Centro histórico de Morelia"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reporte_bache)
@@ -99,6 +121,12 @@ class reporte_bache : AppCompatActivity() {
         btCancelar.setOnClickListener(View.OnClickListener {
             finish()
         })
+
+        //Obtener la ubicacion del usuario, primero en coordenada, posteriormente
+        //se transforman a texto y se muestran en el campo de ubicacion.
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        geocoder = Geocoder(this, Locale.getDefault())
+        getLocation()
     }
 
     //---------------- Función para mandar los datos a la base de datos
@@ -153,4 +181,68 @@ class reporte_bache : AppCompatActivity() {
         val ref = findViewById<EditText>(R.id.etReferenciasAP)
         ref.setText(null)
     }
+
+    private fun getLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        val latitude = location.latitude
+                        val longitude = location.longitude
+                        val locationText = "Lat: $latitude, Long: $longitude"
+                        var fullAddress = geocoder.getFromLocation(latitude, longitude, 1)
+                        showToast(locationText)
+                        println("Locale: ${fullAddress?.get(0)?.locale}")
+                        println("Locality: ${fullAddress?.get(0)?.locality}")
+                        println("Address: ${fullAddress?.get(0)?.getAddressLine(0)}")
+                        println("SubLocality: ${fullAddress?.get(0)?.subLocality}")
+                        println("SubAdminArea: ${fullAddress?.get(0)?.subAdminArea}")
+                        println("Premises: ${fullAddress?.get(0)?.premises}")
+                        println("SubThroughfare: ${fullAddress?.get(0)?.subThoroughfare}")
+                        println("Throughfare: ${fullAddress?.get(0)?.thoroughfare}")
+                        calle = fullAddress?.get(0)?.thoroughfare + fullAddress?.get(0)?.subThoroughfare
+                        colonia = fullAddress?.get(0)?.subLocality
+                    } else {
+                        showToast("No se pudo obtener la ubicación")
+                    }
+                }
+                .addOnFailureListener { e ->
+                    showToast("Error al obtener la ubicación: ${e.message}")
+                }
+        } else {
+            // Permission not granted, request the permission
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Location permission granted, proceed to get location
+                getLocation()
+            } else {
+                // Location permission denied, handle the scenario accordingly
+                showToast("Location permission denied")
+            }
+        }
+    }
+
+
 }
